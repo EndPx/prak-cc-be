f653f613e703599c772c2543601143e5c6459edf
EndPx <albary6700@gmail.com>
2025-05-19 10:18:21 +0700

    fix variable name db

--- index.js
+++ index.js
@@ -5,12 +5,6 @@ const UserRoute = require("./routes/UserRoute.js");
 
 const app = express();
 
-app.use(cors({
-  origin: "http://localhost:3000",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-}));
 app.use(express.json());
 app.use(NoteRoute);
 app.use("/users",UserRoute);
