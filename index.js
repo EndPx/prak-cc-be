dce701b7524c8a7c6bf80462b01a4943300b5835
EndPx <albary6700@gmail.com>
2025-05-25 10:52:05 +0700

    repair index

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const UserRoute = require("./routes/UserRoute.js");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 app.use(NoteRoute);
 app.use("/users",UserRoute);
